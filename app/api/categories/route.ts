ba1ccb6ea8e382bfd8abfb0a34a24413121a4b1d
Thanh Pham <thanhagar123@gmail.com>
2026-01-27 12:54:44 +0700

    PUPS-19 Create Task and Category API (CRUD)

---
+++ app/api/categories/route.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server";
+import { auth } from "@/app/lib/auth";
+import { Category } from "@/app/lib/models/Category";
+import { connectDB } from "@/app/lib/db";
+
+export async function GET() {
+  await connectDB();
+
+  const user = await auth();
+  if (!user) {
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+  }
+
+  const categories = await Category.find().sort({ createdAt: -1 });
+  return NextResponse.json(categories);
+}
